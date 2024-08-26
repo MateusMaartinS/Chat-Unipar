8714cc5ddc3f616896163f5a3330e20a70a28e02
Mateus Martins S. <108599241+MateusMaartinS@users.noreply.github.com>
2024-08-26 20:52:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,87 +1,100 @@
 var stompClient = null;
 var username = null;
+var userColors = {};
 
 document.getElementById("welcome-form").style.display = "block";
 
-    function enterChatRoom() {
-        username = document.getElementById("username").value.trim();
+function enterChatRoom() {
+    username = document.getElementById("username").value.trim();
+
+    if (username) {
+        var welcomeForm = document.getElementById("welcome-form");
+        welcomeForm.classList.add('hide');
+        setTimeout(() => {
+            welcomeForm.style.display = 'none';
+            var chatRoom = document.getElementById('chat-room');
+            chatRoom.style.display = 'block';
+            setTimeout(() => { chatRoom.classList.add('show'); }, 10);
+        }, 550);
+
+        userColors[username] = getRandomColor();
+        connect();
+    } else {
+        alert("Por favor, insira um nickname!");
+    }
+}
+
+function connect() {
+    var socket = new SockJS('http://9e7df17fa210.ngrok.app/chat-websocket', {
+        headers: {
+            'ngrok-skip-browser-warning': 'true'
+        }
+    });
+    stompClient = Stomp.over(socket);
+
+    stompClient.connect({}, function (frame) {
+        console.log('conectando...' + frame);
+
+        stompClient.subscribe('/topic/public', function (messageOutput) {
+            showMessage(JSON.parse(messageOutput.body));
+        });
+
+        stompClient.send("/app/addUser", {}, JSON.stringify({
+            sender: username,
+            type: 'JOIN'
+        }));
+    });
+}
+
+function showMessage(message) {
+    var messageElement = document.createElement('div');
     
-        if(username){
-            var welcomeForm = document.getElementById("welcome-form");
-            welcomeForm.classList.add('hide');
-            setTimeout(() => {
-                welcomeForm.style.display = 'none';
-                var chatRoom = document.getElementById('chat-room');
-                chatRoom.style.display = 'block';
-                setTimeout(() => { chatRoom.classList.add('show'); }, 10);
-            }, 550);
-            connect();
-        } else {
-            alert("Por favor, inserir um nickname");
-        }
+    if (!(message.sender in userColors)) {
+        userColors[message.sender] = getRandomColor();
     }
 
-    function connect() {
+    var color = userColors[message.sender] || '#000000';
 
-        var socket = new SockJS('https://75a5d5e89ffe.ngrok.app/chat-websocket', {
-            headers: {
-                'ngrok-skip-browser-warning': 'true'
-            }
-        });
-        stompClient = Stomp.over(socket);
-
-        stompClient.connect({}, function (frame){
-            console.log('conectando...'+ frame);
-
-            stompClient.subscribe('/topic/public', function(messageOutput) {
-                showMessage(JSON.parse(messageOutput.body));
-            });
-
-            stompClient.send("/app/addUser", {}, JSON.stringify({
-                sender: username,
-                type :'JOIN'
-            }));
-        });
-    }
-    function showMessage(message){
-        var messageElement = document.createElement('div');
-
-        if(message.type === 'JOIN'){
-            messageElement.innerText = message.sender + " entrou na sala ";
-        }else if(message.type === 'LEAVE') {
-            messageElement.innerText = message.sender + " saiu da sala ";
-        }else {
-            messageElement.innerText = message.sender + " disse: " + message.content;
-        }    
-     
-     document.getElementById('messages').appendChild(messageElement);
+    if (message.type === 'JOIN') {
+        messageElement.innerHTML = `<span style="color:${color};">${message.sender}</span> entrou na sala`;
+    } else if (message.type === 'LEAVE') {
+        messageElement.innerHTML = `<span style="color:${color};">${message.sender}</span> saiu da sala`;
+    } else {
+        messageElement.innerHTML = `<span style="color:${color};">${message.sender}</span> disse: ${message.content}`;
     }
 
-    function sendMessage(){
-        var messageContent = document.getElementById("messageInput").value.trim();
+    document.getElementById('messages').appendChild(messageElement);
+    var messagesContainer = document.getElementById('messages');
+    messagesContainer.appendChild(messageElement);
 
-        if(messageContent && stompClient){
-            var chatMessage = {
-                sender: username,
-                content: messageContent,
-                type: 'CHAT'
-            };
-            stompClient.send('/app/sendMessage',{},JSON.stringify(chatMessage));
-            document.getElementById("messageInput").value = '';
-        }
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+}
+
+function sendMessage() {
+    var messageContent = document.getElementById("messageInput").value.trim();
+
+    if (messageContent && stompClient) {
+        var chatMessage = {
+            sender: username,
+            content: messageContent,
+            type: 'CHAT',
+            color: userColors[username] 
+        };
+        stompClient.send('/app/sendMessage', {}, JSON.stringify(chatMessage));
+        document.getElementById("messageInput").value = '';
     }
+}
 
-    function leaveChat(){
-        if (stompClient){
-            var chatMessage = {
-                sender: username,
-                type: 'LEAVE'
-            };
-        stompClient.send("/app/leaveUser",{},JSON.stringify(chatMessage));
-        stompClient.disconnect(()=>{
+function leaveChat() {
+    if (stompClient) {
+        var chatMessage = {
+            sender: username,
+            type: 'LEAVE'
+        };
+        stompClient.send("/app/leaveUser", {}, JSON.stringify(chatMessage));
+        stompClient.disconnect(() => {
             console.log("Desconectado");
 
-        
             var chatRoom = document.getElementById("chat-room");
             chatRoom.classList.remove('show');
             setTimeout(() => {
@@ -89,10 +102,39 @@ document.getElementById("welcome-form").style.display = "block";
                 var welcomeForm = document.getElementById('welcome-form');
                 welcomeForm.style.display = 'block';
                 setTimeout(() => { welcomeForm.classList.remove('hide'); }, 10);
-            }, 550); 
+            }, 550);
         });
+    }
+}
 
+document.getElementById("messageInput").addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        sendMessage();
+    }
+});
 
-        
+document.getElementById("username").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        enterChatRoom(); 
+    }
+});
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        event.preventDefault();
+        if (document.getElementById('chat-room').style.display === 'block') {
+            leaveChat();
         }
     }
+});
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
